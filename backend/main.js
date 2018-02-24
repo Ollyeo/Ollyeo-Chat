82732df394b8084dc80260da382bb89d670fc178
Park Hyun Seo <hyeonseo9669@hanmail.net>
2018-02-24 14:56:14 +0000

    initialize commit

---
+++ backend/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('hapi');
+const socket = require('socket.io');
+
+// Create a server with a host and port
+const server = Hapi.server({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+const io = socket(server);
+
+// web socket setting.
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello', 
+    handler: function (request, h) {
+
+        return 'hello world';
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
